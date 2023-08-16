b65cf260c58bb021fbc460280858ebe7b01372a6
BukeeRuth <olatunjibukola018@gmail.com>
2023-08-15 19:12:19 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,45 +8,38 @@
 
 void print_times_table(int n)
 {
-int num, mult, prod;
-if (n >= 0 && n <= 15)
-{
-for (num = 0; num <= n; num++)
-{
-for (mult = 0; mult <= n; mult++)
-{
-prod = num * mult;
+	int i, j, k;
 
-if (mult != 0)
-{
-_putchar(',');
-_putchar(' ');
-}
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
 
-if (prod < 10)
-_putchar(' ');
+			for (j = 1; j <= n; j++)
+			{
+				_putchar(',');
+				_putchar(' ');
 
-if (prod < 100)
-_putchar(' ');
+				k = i * j;
 
-if (prod == 0)
-{
-_putchar('0');
-}
-else if (prod < 10)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(prod + '0');
-}
-else
-{
-_putchar((prod / 100) + '0');
-_putchar(((prod / 10) % 10) + '0');
-_putchar((prod % 10) + '0');
-}
-}
-_putchar('\n');
-}
-}
+				if (k <= 99)
+					_putchar(' ');
+				if (k <= 9)
+					_putchar(' ');
+
+				if (k >= 100)
+				{
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10)) % 10 + '0');
+				}
+				else if (k <= 99 && k >= 10)
+				{
+					_putchar((k / 10) + '0');
+				}
+				_putchar((k % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
 }
