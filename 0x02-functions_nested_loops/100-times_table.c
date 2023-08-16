25022faca4d8eb298bcaf068a83e63d1a010a176
BukeeRuth <olatunjibukola018@gmail.com>
2023-08-15 19:03:37 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,7 +30,15 @@ if (prod < 100)
 _putchar(' ');
 
 if (prod == 0)
+{
 _putchar('0');
+}
+else if (prod < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(prod + '0');
+}
 else
 {
 _putchar((prod / 100) + '0');
