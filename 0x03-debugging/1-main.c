da3838a2386619e37e47555ab9a1a6fe1b145764
BukeeRuth <olatunjibukola018@gmail.com>
2023-08-17 07:11:39 -0700

    positive

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - outputs an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("Warning! Infinite loop :(\n");
+
+	i = 0;
+
+	/*while (i < 10) */
+	/*{ */
+	/*      putchar(i);*/
+	/*} */
+
+	printf("Broken out of infinite loop \\o/\n");
+
+	return (0);
+}
