6e2ad2c1d357c36ba103e512fd4761934cf3c598
BukeeRuth <olatunjibukola018@gmail.com>
2023-08-17 16:08:18 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,26 +13,26 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" FIzzbuzz");
+			printf("FIzzbuzz ");
 		}
 
 		else if (i % 3 == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz ");
 		}
 
 		else if (i % 5 == 0)
 		{
-			printf(" Buzz ");
+			printf("Buzz ");
 		}
 
 		else
 		{
-			printf(" %d", i);
+			printf("%d ", i);
 		}
 	}
 
