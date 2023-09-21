b4cc7cb5466f6cc820516e50e9599189bced984c
BukeeRuth <olatunjibukola018@gmail.com>
2023-09-21 16:19:43 -0700

    asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,23 @@
 section .data
     hello db "Hello, Holberton",10  ; The string to be printed, followed by a newline
     hello_len equ $ - hello         ; Calculate the length of the string
-    format db "%s", 0              ; Format string for printf
 
 section .text
     global main                     ; Entry point for the program
     extern printf                  ; Declare printf as an external symbol
 
 main:
-    ; Prepare arguments for printf
-    mov rdi, format                ; Address of the format string
-    mov rsi, hello                 ; Address of the string to print
-    xor rax, rax                   ; Clear RAX register (no floating-point args)
-    call printf                    ; Call printf function
+    ; Load the address of the format string into RDI
+    mov rdi, hello
+
+    ; Load the address of the string to print into RSI
+    mov rsi, hello
+
+    ; Clear RAX register (no floating-point args)
+    xor rax, rax
+
+    ; Call printf function
+    call printf
 
     ; Exit the program
     mov rax, 60                    ; syscall number for exit (60 on x86-64)
