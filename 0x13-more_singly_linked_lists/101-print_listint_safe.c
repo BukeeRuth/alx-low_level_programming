26af8cc6e17eee2428d4ba37a827406acb909209
BukeeRuth <olatunjibukola018@gmail.com>
2023-09-26 18:42:43 -0700

    print list safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,24 +9,34 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current = head;
+	const listint_t *temp = NULL;
+	const listint_t *len = NULL;
 	size_t count = 0;
+	size_t new_node;
 
-	while (current != NULL)
+	temp = head;
+	while (temp)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
+		printf("[%p] %d\n", (void *)temp, temp->n);
 		count++;
-
-		/* Check if the next pointer points back to a node we've visited before */
-		if (current <= current->next)
+		temp = temp->next;
+		len = head;
+		new_node = 0;
+		while (new_node < count)
 		{
-			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-			break;
+			if (temp == len)
+			{
+				printf("-> [%p] %d\n", (void *)temp, temp->n);
+				return (count);
+			}
+			len = len->next;
+			new_node++;
 		}
-
-		current = current->next;
+		if (!head)
+		{
+			exit(98);
+		}
 	}
-
 	return (count);
 }
 
